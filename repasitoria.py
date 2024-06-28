74e05122121bbb07ca96cf7c7f9e0966386fd971
Vazgen <asryan-vazgen@mail.ru>
2024-06-28 15:28:28 +0400

    new_1

--- repasitoria.py
+++ repasitoria.py
@@ -1,8 +1,23 @@
-print('Hello world')
+def arabic_to_roman(num):
+    if num <= 0:
+        raise ValueError("Number must be greater than 0")
 
-print(10 + 25)
-print(10)
+    roman_numerals = [
+        (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
+        (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'),
+        (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'),
+        (1, 'I')
+    ]
 
+    roman_result = ""
+    for value, numeral in roman_numerals:
+        while num >= value:
+            roman_result += numeral
+            num -= value
 
+    return roman_result
 
-print(51000)
+# Test cases
+print(arabic_to_roman(10))  # Expected output: XV
+print(arabic_to_roman(72))  # Expected output: LXXII
+print(arabic_to_roman(9))   # Expected output: IX
